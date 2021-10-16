acdc720d47747e793a83d038fdee61499787ce35
Ivan Rapisarda <ivanrapisarda7@gmail.com>
2021-10-16 17:43:38 -0300

    Servidores web

--- test.js
+++ test.js
@@ -1,11 +1,12 @@
-const DataBase = require('./index')
+const DataBase = require('./main')
 
 const db = new DataBase('productos.txt')
 
 const test = async() => {
 //console.log( await db.obtenerTodo())
-//console.log( await db.guardarProducto({nombre:'Xbox One', precio: '1000USD', img:'https://compass-ssl.xbox.com/assets/97/69/9769090c-3d1d-4833-a935-5137fc7d6402.jpg?n=111_Content-Placement-0_Console-Controller_788x444.jpg'}))
-//console.log( await db.guardarProducto({nombre:'Playstation 5', precio: '1500USD', img:'https://arsonyb2c.vteximg.com.br/arquivos/menu-ps5.png?v=637300051469000000'}))
+//console.log( await db.guardarProducto({nombre:'Xbox One', precio: '1000', img:'https://compass-ssl.xbox.com/assets/97/69/9769090c-3d1d-4833-a935-5137fc7d6402.jpg?n=111_Content-Placement-0_Console-Controller_788x444.jpg'}))
+//console.log( await db.guardarProducto({nombre:'Playstation 5', precio: '1500', img:'https://arsonyb2c.vteximg.com.br/arquivos/menu-ps5.png?v=637300051469000000'}))
+//console.log( await db.guardarProducto({nombre:'Pc Gamer', precio: '2000', img:'https://http2.mlstatic.com/D_NQ_NP_777167-MLA46258832185_062021-O.webp'}))
 ////console.log( await db.obtenerPorId(1))
 //await db.borrarTodo()
 //console.log ( await db.obtenerTodo())
